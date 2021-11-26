0a93b088a9056973aafb90c18916f48efc0b7243
Hyunsu Joo <hs9880@gmail.com>
2021-11-26 12:27:52 +0900

    :necktie: REFACTOR: 상품서치기능분리

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,19 +1,38 @@
-import { Button } from "@chakra-ui/button";
-import { Heading } from "@chakra-ui/layout";
 import * as React from "react";
 import { Categories } from "../components/organisms/Categories";
+import { SearchInput } from "../components/organisms/SearchInput";
 import { ProductLists } from "../components/templates/ProductLists";
+import { SearchResult } from "../components/templates/SearchResult";
+import { defaultConfig } from "../defaultConfig";
 
 type HomeProps = {
   children?: React.ReactNode;
 };
 
 const Home: React.FunctionComponent = ({ children }: HomeProps) => {
+  const [categoryId, setCategoryId] = React.useState(
+    defaultConfig.defaultCategoryId
+  );
+
+  const handleClickCategory = (categoryId) => {
+    setCategoryId(categoryId);
+  };
+
+  const [searchTerm, setSearchTem] = React.useState("");
+
+  const handleSarchInput = (e) => {
+    let value = e.target.value.toLowerCase();
+    setSearchTem(value);
+  };
   return (
     <>
-      <Heading>Home.Product page</Heading>
-      <Categories />
-      <ProductLists />
+      <Categories handleClickCategory={handleClickCategory} />
+      <SearchInput handleSearchInput={handleSarchInput} />
+      {searchTerm ? (
+        <SearchResult categoryId={categoryId} searchTerm={searchTerm} />
+      ) : (
+        <ProductLists categoryId={categoryId} />
+      )}
     </>
   );
 };
