b9ce675d17f9b9ca0a76568b9d58bd55f1331180
Hyunsu Joo <hs9880@gmail.com>
2021-11-26 12:26:35 +0900

    :lipstick: UPDATE: 가격 및 아이템정보 추가

--- src/components/templates/ProductLists.tsx
+++ src/components/templates/ProductLists.tsx
@@ -1,24 +1,19 @@
 import { Button } from "@chakra-ui/button";
-import { Badge } from "@chakra-ui/layout";
-import { useStyleConfig } from "@chakra-ui/system";
+import { Text } from "@chakra-ui/react";
 import * as React from "react";
 import { useQuery } from "react-query";
 import { List } from "../../layout/List";
 import { SoldOutBadge } from "../atoms/SoldOutBadge";
 import { Card } from "../organisms/Card";
-
-interface IProductListsProps {}
-
-const fetchProductsByCategory = async (categoryId = 11, page = 1) => {
-  const resp =
-    await fetch(`https://mock-api.ssomee.com/products/${categoryId}/${page}?order=date-desc
-`);
-  return resp.json();
+import { DefalutSpinner } from "../atoms/DefaultSpinner";
+import { Box, Stat, StatGroup, StatHelpText } from "@chakra-ui/react";
+import { formatProductPrice } from "../../utils/formatProductPrice";
+import { fetchProductsByCategory } from "../../api/productAPI";
+type ProductListsProps = {
+  categoryId?: number;
 };
-export const ProductLists: React.FunctionComponent<IProductListsProps> = (
-  props
-) => {
-  const [categoryId, setCategoryId] = React.useState(11);
+
+export const ProductLists = ({ categoryId }: ProductListsProps) => {
   const [page, setPage] = React.useState(1);
   const { isLoading, isError, data, status } = useQuery(
     ["category", categoryId, page],
@@ -27,46 +22,72 @@ export const ProductLists: React.FunctionComponent<IProductListsProps> = (
       keepPreviousData: true,
     }
   );
-  const isNextDisabled = (curPage: number) => curPage === data?.maxPage;
-  const isPrevDisabled = (curPage: number) => curPage <= 1;
+  const isNextDisabled = (curPage: number) =>
+    curPage === data?.maxPage || data?.maxPage === 0;
+
   return (
     <>
-      <h2>Products Lists</h2>
-      {isLoading && <div>Loading data...</div>}
-      {isError && <div>Error fetching data</div>}
+      {isLoading && <DefalutSpinner />}
+
+      {isError && <span>Error: Fail to fetch data </span>}
       {status === "success" && (
         <>
+          <Text fontSize="2xl">{data?.category?.name}</Text>
           <List
             items={data?.products}
             renderEmpty={<p> 상품이 없습니다.</p>}
             renderItem={(item) => (
               <Card
+                id={item.prefix}
                 imageUrl={item.mainImage}
                 imageAlt={item.name}
                 title={item.name}
                 badge={item.soldOut ? <SoldOutBadge /> : null}
                 tag={null}
                 detail={item.brand?.name}
-              />
+              >
+                {item.originalPrice && item.ssomeePrice && (
+                  <StatGroup>
+                    <Stat>
+                      <StatHelpText as="del">
+                        original price
+                        {formatProductPrice(item.originalPrice)}
+                      </StatHelpText>
+                    </Stat>
+                    <Stat>
+                      <StatHelpText>
+                        ssomee price
+                        {formatProductPrice(item.ssomeePrice)}
+                      </StatHelpText>
+                    </Stat>
+                  </StatGroup>
+                )}
+              </Card>
             )}
           ></List>
 
-          <Button
-            onClick={() => setPage((old) => Math.max(old - 1, 1))}
-            disabled={page === 1}
-          >
-            Previous Page
-          </Button>
-          <span>Current Page: {page}</span>
-          <Button
-            onClick={() => {
-              setPage((old) => old + 1);
-            }}
-            // Disable the Next Page button until we know a next page is available
-            disabled={isNextDisabled(page)}
-          >
-            Next Page
-          </Button>
+          <Box textAlign={"center"} margin={"2rem auto"}>
+            <Button
+              mr={2}
+              onClick={() => setPage((old) => Math.max(old - 1, 1))}
+              disabled={page === 1}
+            >
+              Previous Page
+            </Button>
+            <span>
+              Current Page: {page} of {data?.maxPage}
+            </span>
+            <Button
+              ml={2}
+              onClick={() => {
+                setPage((old) => old + 1);
+              }}
+              // Disable the Next Page button until we know a next page is available
+              disabled={isNextDisabled(page)}
+            >
+              Next Page
+            </Button>
+          </Box>
         </>
       )}
     </>
