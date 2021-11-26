da45309009a6d8d03f6906af86a562206020e6f7
Hyunsu Joo <hs9880@gmail.com>
2021-11-26 12:31:54 +0900

    :wastebasket: REMOVE :불필요한로그

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,3 @@
 export const isDeepEqual = (obj_A, obj_B) => {
-    console.log("isDeepEqual",JSON.stringify(obj_A)=== JSON.stringify(obj_B))
     return JSON.stringify(obj_A)=== JSON.stringify(obj_B);
 }
